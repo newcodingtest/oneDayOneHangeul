b4fed8a254f01aa2b9dcf0c018ea1c32d6debc1f
yoon ju young <pulpul8282@naver.com>
2026-02-13 18:36:00 +0900

    ref: mp3 호출 메서드 수정

--- app-main/app/api/tts/generate/route.ts
+++ app-main/app/api/tts/generate/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest){
   const { grammarData, mp3DataKey } = await request.json();
@@ -9,4 +9,10 @@ export async function POST(request: NextRequest){
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ grammarData: grammarData, mp3DataKey: "2026_2_13"}),
         });
+// 3. 백엔드의 응답을 받아옵니다.
+    const result = await ttsResponse.json();
+
+    // 4. ⭐ 중요: 브라우저에게 최종 결과를 반환합니다.
+    // 이렇게 return을 해줘야 브라우저는 Vercel 서버하고만 대화한 셈이 됩니다.
+    return NextResponse.json(result);
 }
