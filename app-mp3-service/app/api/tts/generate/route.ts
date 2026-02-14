d05b3fd8e3684af6cb118a63ab661eb746b227a1
newcodingtest <pulpul8282@naver.com>
2026-02-14 13:09:35 +0900

    test: mp3 생성 로그 추가

--- app-mp3-service/app/api/tts/generate/route.ts
+++ app-mp3-service/app/api/tts/generate/route.ts
@@ -3,7 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest){
   const { grammarData, mp3DataKey } = await request.json();
-
+  
+  console.log(`make mp3DataKey: ${mp3DataKey}`);
   try{
     await generateTTSForLesson(grammarData, mp3DataKey);
     return NextResponse.json({
